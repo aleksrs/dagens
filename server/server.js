fd81cb068973b0e9c086ba85f5b571c970740e99
Aleksander Røe Strømshoved <aleksroe@broadpark.no>
2021-01-26 21:33:54 +0100

    ✨ Get 10 closest products on price
    Functionality to get the 10 closest products

--- server/server.js
+++ server/server.js
@@ -25,11 +25,26 @@ app.get('/', (req, res, next) => {
   res.send(resultsToReturn);
 })
 
+app.get('/findClosest/:id', (req, res, next) => {
+  const product = read(req.params.id);
+  const productsInCategory = listProducts(product.category, 0, 0)
+    .sort((a, b) => {
+      if (a.price < b.price) return -1;
+      if (a.price > b.price) return 1;
+      return 0;
+    });
+
+  const productsToReturn = findClosestInPrice(productsInCategory, product, 10);
+
+  res.send(productsToReturn);
+})
+
 app.post('/', (req, res, next) => {
   const newProduct = create(req.body);
   res.send(newProduct);
 });
 
+// Controllers
 const create = (product) => {
   product.id = uuidv4();
   products.push(product);
@@ -38,6 +53,7 @@ const create = (product) => {
 
 const read = (productID) => products.find(product => product.id === productID);
 
+// Getters
 const listProducts = (category, minPrice, maxPrice) => {
   return products.filter((product) => {
     if (category && minPrice && maxPrice) return product.category === category && product.price >= minPrice && product.price <= maxPrice;
@@ -49,6 +65,43 @@ const listProducts = (category, minPrice, maxPrice) => {
 
     return product;
   })
+}
+
+// Helpers
+const findClosestInPrice = (sortedList, initialProduct, numElements) => {
+  const results = [];
+
+  const initialPrice = initialProduct.price;
+  let left = sortedList.indexOf(initialProduct) - 1;
+  let right = left + 2;
+  let counter = 0;
+
+  while (left >= 0 && right < sortedList.length && counter < numElements) {
+    const leftDiff = initialPrice - sortedList[left].price
+    const rightDiff = sortedList[right].price - initialPrice;
+
+    if (leftDiff < rightDiff) {
+      results.push(sortedList[left]);
+      left--;
+    }
+    else {
+      results.push(sortedList[right]);
+      right++;
+    }
+    counter ++;
+  }
+
+  while (counter < numElements && left >= 0) {
+    results.push(sortedList[left]);
+    left--;
+  }
+
+  while (counter < numElements && right < sortedList.length) {
+    results.push(sortedList[right]);
+    right++;
+  }
+
+  return results;
 }
 
 const paginate = (items, page) => {
